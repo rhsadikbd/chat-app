7cda4f18f0f7071c49ffda2aeb8828a0452b478c
sadikbd <sadikcsbd@gmail.com>
2023-03-11 11:37:14 +0600

    chatting ap design un=sing vue3

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <chat-app />
+</template>
+
+<script>
+import Chat from './components/Chat.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'chat-app': Chat
+  }
+}
+</script>
+
+<style>
+  body{
+    background: #6c5ce7;
+    height: 98vh;
+    font-family: sans-serif;
+  }
+</style>
